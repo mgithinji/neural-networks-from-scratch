1bc261333930c127343ebc86d06e356d3792ec2f
Morgan Githinji <githinjimorgan@gmail.com>
2024-07-24 16:53:25 -0400

    added backward methods for the Softmax and Loss classes; new combined Softmax and Loss class

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -44,6 +44,18 @@ class Softmax(Activation):
         prob_norm = prob_raw = prob_raw / np.sum(prob_raw, axis=1, keepdims=True)
         self.output = prob_norm
         
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues) # uninitialized array
+        
+        for i, (i_output, i_dvalues) in enumerate(zip(self.output, dvalues)):
+            # creating the Jacobian matrix of the partial derivatives of the softmax function
+            i_output = i_output.reshape(-1, 1)
+            jacobian = np.diagflat(i_output) - np.dot(i_output, i_output.T)
+            
+            # calculating the sample-wise gradient and adding it to the sample gradients
+            self.dinputs[i] = np.dot(jacobian, i_dvalues)
+            
+        
 # base Loss class
 class Loss(ABC):
     # forward pass
@@ -74,6 +86,40 @@ class CategoricalCrossEntropyLoss(Loss):
         negative_log_likelihoods = -np.log(target_confidence_scores)
         return negative_log_likelihoods
     
+    def backward(self, dvalues, y_true):
+        n_samples = len(dvalues) # number of samples
+        n_labels = len(dvalues[0]) # number of labels in each sample
+        
+        # adding a condition to turn sparse targets into one-hot encoded vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(n_labels)[y_true]
+            
+        self.dinputs = -y_true / dvalues # calculate gradient
+        self.dinputs = self.dinputs / n_samples # normalize gradient
+
+# combined Softmax and CCE loss class
+class SoftmaxActivationCCELoss():
+    def __init__(self) -> None:
+        self.activation = Softmax()
+        self.loss = CategoricalCrossEntropyLoss()
+        
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs=inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(output=self.activation.output, y=y_true)
+    
+    def backward(self, dvalues, y_true):
+        n_samples = len(dvalues) # number of samples
+        
+        # if labels are one-hot encoded, turn them into discrete values
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+            
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(n_samples), y_true] -= 1 # calculating gradient
+        self.dinputs = self.dinputs / n_samples # normalizing gradient
+        
+
 def calculate_accuracy(output, y):
     # get the predictions in a single vector
     predictions = np.argmax(output, axis=1)

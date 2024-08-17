2a640c6599f7b59633e085b1ab95a3f2548d4e34
Morgan Githinji <githinjimorgan@gmail.com>
2024-08-17 17:48:15 -0400

    completed training and accuracy calculation methods for model object

--- Model.py
+++ Model.py
@@ -29,9 +29,10 @@ class Model:
         self.layers.append(layer)
         
     # set loss and optimizer
-    def set(self, *, loss, optimizer):
+    def set(self, *, loss, optimizer, accuracy):
         self.loss = loss
         self.optimizer = optimizer
+        self.accuracy = accuracy
         
     # finalize the model
     def finalize(self):
@@ -59,9 +60,12 @@ class Model:
                 self.layers[i].next = self.loss
                 self.output_layer_activation = self.layers[i]
                 
-            # tracking trainable layers
+            # tracking trainable layers -- those that have a weight attribute
             if hasattr(self.layers[i], 'weights'):
                 self.trainable_layers.append(self.layers[i])
+                
+        # updating loss object with trainable layers
+        self.loss.remember_trainable_layers(self.trainable_layers)
     
     # perform a forward pass
     def forward(self, X):
@@ -70,20 +74,78 @@ class Model:
         for layer in self.layers:
             layer.forward(layer.prev.output)
         
-        # layer is now last object from the list, return its output
+        # "layer" is now last object from the list, return its output
         return layer.output
+    
+    # perform a backward pass
+    def backward(self, output, y):
+        # start by calling backward method on the loss to set dinputs for the last layer to use
+        self.loss.backward(output, y)
+        
+        # call backward method going through all network objects in reverse
+        for layer in reversed(self.layers):
+            layer.backward(layer.next.dinputs)
     
     # training the model
     def train(self, X, y, *, epochs=1, print_every=1):
+        # initialize accuracy object
+        self.accuracy.init(y)
+        
         # main training in loop
         for epoch in range(1, epochs + 1):
             
             # perform the forward pass
             output = self.forward(X)
             
-            print(output)
-            exit()
+            # calculate loss
+            data_loss, regularization_loss = self.loss.calculate(output, y)
+            loss = data_loss + regularization_loss
+            
+            # get predictions and calculate accuracy
+            predictions = self.output_layer_activation.predictions(output)
+            accuracy = self.accuracy.calculate(predictions, y)
+            
+            # perform backward pass
+            self.backward(output, y)
+            
+            # optimize / update params
+            self.optimizer.pre_update_params()
+            for layer in self.trainable_layers:
+                self.optimizer.update_params(layer)
+            self.optimizer.post_update_params()
+            
+            # print training summary
+            if not epoch % print_every:
+                print(f"epoch: {epoch}, " + 
+                      f"acc: {accuracy:.3f}, " + 
+                      f"loss: {loss:.3f} (data loss: {data_loss:.3f}, reg loss: {regularization_loss:.3f}), " +
+                      f"lr: {self.optimizer.current_learning_rate:.5f}")
+            
+            
     
+# creating accuracy classes
+
+# base accuracy class
+class Accuracy:
+    # calculating accuracy
+    def calculate(self, predictions, y):
+        comparisons = self.compare(predictions, y)
+        accuracy = np.mean(comparisons)
+        return accuracy
+    
+# accuracy for regression model
+class RegressionAccuracy(Accuracy):
+    def __init__(self) -> None:
+        self.precision = None
+    
+    # calculating the precision value
+    def init(self, y, reinit=False):
+        if self.precision is None or reinit:
+            self.precision = np.std(y) / 250
+            
+    # comparing predictions and ground truth
+    def compare(self, predictions, y):
+        return np.absolute(predictions - y) < self.precision
     
     
 # instantiate the model
@@ -99,12 +161,11 @@ model.add(Linear())
 
 # set loss and optimizer objects
 model.set(loss=MeanSquaredErrorLoss(),
-          optimizer=Adam(learning_rate=0.005, decay=1e-3))
+          optimizer=Adam(learning_rate=0.005, decay=1e-3),
+          accuracy=RegressionAccuracy())
 
 # finalize the model
 model.finalize()
 
 # training the model
 model.train(X, y, epochs=10000, print_every=1000)
-
-print(model.layers)

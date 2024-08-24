c704b9fd5c8b0cc5f7736b043f8352d12c7b6e3b
Morgan Githinji <githinjimorgan@gmail.com>
2024-08-24 18:05:55 -0400

    pulling mnist dataset into python

---
+++ DataLoading.py
@@ -0,0 +1,37 @@
+# reading image data and associating images with labels
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.set_printoptions(linewidth=200)
+
+def load_mnist_dataset(dataset, path):
+    # scanning all directories and creating a list of labels
+    labels = os.listdir(os.path.join(path, dataset))
+
+    # creating lists for samples and labels
+    X = []
+    y = []
+
+    # moving samples and labels into our lists
+    for label in labels:
+        for file in os.listdir(os.path.join(path, dataset, label)):
+            image = cv2.imread(os.path.join(path, dataset, label, file), cv2.IMREAD_UNCHANGED)
+            X.append(image)
+            y.append(label)
+            
+    return np.array(X), np.array(y).astype('uint8')
+
+def create_mnist_dataset(path):
+    # load both datasets separately
+    X, y = load_mnist_dataset("train", path)
+    X_test, y_test = load_mnist_dataset("test", path)
+    
+    # return all
+    return X, y, X_test, y_test
+
+X, y, X_test, y_test = create_mnist_dataset("fashion_mnist_images")
+
+plt.imshow(X[0])
+plt.show()
